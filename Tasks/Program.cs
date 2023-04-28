8871802c9e5d90a4cf979ed8bb01c18d3520992d
Maksim Komarov <komarovmu@gmail.com>
2023-04-28 16:44:42 +0100

    Block-Scheme done

---
+++ Tasks/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите элементы массива через запятую:");
+        string[] input = Console.ReadLine().Split(',');
+        string[] output = new string[input.Length];
+        int j = 0;
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i].Length <= 3)
+            {
+                output[j] = input[i];
+                j++;
+            }
+        }
+        Array.Resize(ref output, j);
+        Console.WriteLine(string.Join(",", output));
+    }
+}
